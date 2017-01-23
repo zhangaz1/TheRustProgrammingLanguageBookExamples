4e4ec360745f84c2b8217d73f24e991b745aa3d6
zhangaz1 <zhangaz1@hotmail.com>
2017-01-23 15:17:50 +0800

    add convert: string -> number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -18,6 +19,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim()
+                        .parse()
+                        .expect("Please type a number");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal   => println!("You win!"),        
+    }
 
 }
