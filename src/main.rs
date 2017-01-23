d91b8e17b33722c3780b26b885246b7750ce8061
zhangaz1 <zhangaz1@hotmail.com>
2017-01-23 16:18:11 +0800

    use vec! and for in

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,15 @@ impl Philosopher {
 }
 
 fn main() {
-    let p1 = Philosopher::new("Judith Butler");
-    let p2 = Philosopher::new("Gilles Deleuze");
-    let p3 = Philosopher::new("Karl Marx");
-    let p4 = Philosopher::new("Emma Goldman");
-    let p5 = Philosopher::new("Michel Foucault");
-    println!("hello world");
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+    
+    for p in &philosophers {
+        p.eat();
+    }
 }
