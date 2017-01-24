7376e97da6145d092fee8aeb99bc4fa2a81728fa
zhangaz1 <zhangaz1@hotmail.com>
2017-01-24 14:45:03 +0800

    use vec get

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let index: usize = 2;
+    let v = vec![1, 2, 3];
+
+    match v.get(index) {
+        Some(x) => println!("Item {} is {}", index, x),
+        None => println!("Sorry, this vector is too short."),
+    }
 }
