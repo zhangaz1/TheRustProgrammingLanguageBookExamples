29d00519e3a15299b51f893716032f98242514aa
zhangaz1 <zhangaz1@hotmail.com>
2017-01-24 10:22:09 +0800

    5亿*10

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let handles: Vec<_> = (0..10).map(|_| {
         thread::spawn(|| {
             let mut x = 0;
-            for _ in 0..50_000_000 {
+            for _ in 0..500_000_000 {
                 x += 1
             }
             x
