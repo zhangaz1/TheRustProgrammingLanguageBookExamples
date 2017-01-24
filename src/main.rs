546c9e7026f0e87d40fce714af2ef3e5f1943dca
zhangaz1 <zhangaz1@hotmail.com>
2017-01-24 14:36:12 +0800

    continue label

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    for x in 0..10 {
-        if x % 2 == 0 {
-            continue;
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            println!("{},{}", x, y);
+
+            if x % 2 == 0 {
+                continue 'outer;
+            }
+
+            if y % 2 == 0 {
+                continue 'inner;
+            }
+
+            println!("x: {}, y: {}", x, y);
         }
-        
-        println!("{}", x);
     }
 }
