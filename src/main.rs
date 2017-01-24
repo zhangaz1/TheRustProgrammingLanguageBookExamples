f1bfa59fdaebbf26aed2ef5cd59692955caec9be
zhangaz1 <zhangaz1@hotmail.com>
2017-01-24 10:12:36 +0800

    += to 5000 0000

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in 0..50_000_000 {
+                x += 1
+            }
+            x
+        })
+    })
+    .collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+            h.join().map_err(|_| "Could not join a thread").unwrap()
+        );
+    }
 }
