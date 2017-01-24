c76faf0b1d50c98ed45825462f2c79451d5f82cb
zhangaz1 <zhangaz1@hotmail.com>
2017-01-24 15:07:04 +0800

    moved value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3];
+    {
+        let v2 = v;
+    }
+    println!("{}", v[1]);
+
+    let x = 5;
+    {
+        let x2 = &x;
+    }
+    println!("{}", x);
 }
