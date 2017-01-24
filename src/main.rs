cdc0ad31097458a17f36e8d92af499440b6c5fa3
zhangaz1 <zhangaz1@hotmail.com>
2017-01-24 13:50:19 +0800

    for

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 fn main() {
-    let mut x = 5; // mut x: i32
-    let mut done = false; // mut done: bool
-    while !done {
-        x += x - 3;
-        println!("{}", x);
-        if x % 5 == 0 {
-            done = true;
-        }
+    for x in 0..10 {
+        println!("{}", x); // x: i32
     }
 }
